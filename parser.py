b8ace47b3916d325cb69a658ae5936e83c812ef7
“Liubov <wolf6961@gmail.com>
2021-04-29 22:00:12 +0300

    added files

---
+++ parser.py
@@ -0,0 +1,12 @@
+import requests
+
+
+URL = 'https://tabletki.ua/uk/search/?q='
+
+# https://citrus.ua/search?query=xiaomi&page=2
+
+res = requests.get(URL, params={
+    'query': 'аспирин'
+})
+
+print(res.text)
