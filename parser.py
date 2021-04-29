c27b052ac4358fe6ab6760a6d13dd1e3a89b9d22
“Liubov <wolf6961@gmail.com>
2021-04-29 22:23:03 +0300

    parser change

--- parser.py
+++ parser.py
@@ -2,11 +2,3 @@ import requests
 
 
 URL = 'https://tabletki.ua/uk/search/?q='
-
-# https://citrus.ua/search?query=xiaomi&page=2
-
-res = requests.get(URL, params={
-    'query': 'аспирин'
-})
-
-print(res.text)
